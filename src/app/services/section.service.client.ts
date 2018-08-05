3947a880cb57cf94529b56f936459d59f866206b
Will Angell-James <william.angelljames9@gmail.com>
2018-08-05 13:58:16 -0400

    added fetches for section and user

--- src/app/services/section.service.client.ts
+++ src/app/services/section.service.client.ts
@@ -7,18 +7,39 @@ export class SectionServiceClient {
     fetch('http://localhost:3000/api/section/' + sectionId + '/enroll', {
       method: 'put',
       credentials: 'include'
-    })
+    });
 
   findAllSections = () =>
     fetch('http://localhost:3000/api/section')
       .then(response => response.json());
 
   findSectionsForCourse = courseId =>
-    fetch('http://localhost:3000/api/course/' + courseId + '/section')
+    fetch('http://localhost:3000/api/course/:courseId/' + courseId + '/section')
       .then(response => response.json());
 
+  findSectionByItsItd = sectionId =>
+    fetch('http://localhost:3000/api/section/:sectionId' + sectionId, {
+      method: 'get',
+      credentials: 'include',
+    }).then(response => response.json());
+
+  updateSection = sectionId =>
+    fetch('http://localhost:3000/api/section/:sectionId' + sectionId, {
+      method: 'put',
+      credentials: 'include',
+    });
+
+  deleteSection = sectionId =>
+    fetch('http://localhost:3000/api/section/:sectionId' + sectionId, {
+      method: 'delete',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      }, body: JSON.stringify(sectionId)
+    }).then(response => response.json());
+
   createSection = section =>
-    fetch('http://localhost:3000/api/section', {
+    fetch('http://localhost:3000/api/course/:courseId/section', {
       method: 'post',
       headers: {
         'content-type': 'application/json'
@@ -26,5 +47,5 @@ export class SectionServiceClient {
       credentials: 'include',
       body: JSON.stringify(section)
     })
-      .then(response => response.json())
+      .then(response => response.json());
 }
--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -1,19 +1,55 @@
-
 import {Injectable} from '@angular/core';
 
 @Injectable()
 export class UserServiceClient {
   login = (user) =>
-    fetch('http://localhost:3000/login', {
+    fetch('http://localhost:3000/api/login', {
       method: 'post',
       credentials: 'include',
       headers: {
         'content-type': 'application/json'
       },
       body: JSON.stringify(user)
-    })
+    });
+
   currentUser = () =>
-    fetch('http://localhost:3000/currentUser', {
+    fetch('http://localhost:3000/api/profile', {
+      method: 'get',
       credentials: 'include'
-    }).then(response => response.json())
+    }).then(response => response.json());
+
+  register = (user) =>
+    fetch('http://localhost:3000/api/register', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      }, body: JSON.stringify(user)
+    });
+  logout = () =>
+    fetch('http://localhost:3000/api/logout', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  updateUser = (uid, user) => {
+    fetch('http://localhost:3000/api/profile', {
+      method: 'put',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      }, body: JSON.stringify(user)
+    });
+  }
+  deleteUser = (uid) => {
+    fetch('http://localhost:3000/api/profile', {
+      method: 'delete',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      }, body: JSON.stringify(uid)
+    });
+  }
 }
