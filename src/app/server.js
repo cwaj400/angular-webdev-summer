f959207fbde3590606d88869d30f76203725487f
Will Angell-James <william.angelljames9@gmail.com>
2018-08-10 17:16:57 -0400

    d

--- src/app/server.js
+++ src/app/server.js
@@ -7,8 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ang-webdev-angelljames'));
 
-app.get('/*', function(req,res) {
 
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
   res.sendFile(path.join(__dirname + '/dist/ang-webdev-angelljames/index.html'));
 });
 
